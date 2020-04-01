e688dbc39c05df9da61e076d14842e11cdde3236
matt-winzer <matt.winzer@gmail.com>
2020-03-31 23:24:33 -0600

    added remove from cart button to cart items

--- src/components/CartItem/index.jsx
+++ src/components/CartItem/index.jsx
@@ -3,10 +3,13 @@ import styles from './cart-item.module.scss';
 
 import Card from '../shared/Card';
 
-const CartItem = ({ item }) => (
+const CartItem = ({ item, removeBookFromCart }) => (
   <Card className={styles.cartItem}>
     <p>{item.title}</p>
-    <p>Price: ${item.price}</p>
+    <div className={styles.priceRemove}>
+      <p>Price: ${item.price}</p>
+      <button className={styles.removeButton} type="submit">Remove from Cart</button>
+    </div>
   </Card>
 );
 
