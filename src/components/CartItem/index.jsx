2b95cbc8dab5c7f1b0cb84b01cf432a2f753e63c
matt-winzer <matt.winzer@gmail.com>
2020-03-31 22:36:17 -0600

    fixed typo in CartItem

--- src/components/CartItem/index.jsx
+++ src/components/CartItem/index.jsx
@@ -3,11 +3,11 @@ import styles from './cart-item.module.scss';
 
 import Card from '../shared/Card';
 
-const BookItem = ({ item }) => (
+const CartItem = ({ item }) => (
   <Card className={styles.cartItem}>
     <p>{item.title}</p>
     <p>Price: ${item.price}</p>
   </Card>
 );
 
-export default BookItem;
+export default CartItem;
